78fe11ec584f0192902a9aab8dc9451328d8728f
Narongchai Ngueanchanthong <guy1234im@gmail.com>
2026-07-15 11:48:24 +0700

    upload 2

---
+++ q2_oop.dart
@@ -0,0 +1,80 @@
+//2.1
+import 'dart:async';
+
+abstract class MenuItem {
+  final String name;
+  final double basePrice;
+  MenuItem(this.name, this.basePrice);
+  double price();
+  void show() {
+    print('${name} - ${price()} บาท');
+  }
+}
+
+//2.2
+class Drink extends MenuItem {
+  int toppings;
+  Drink(super.name, super.basePrice, this.toppings);
+  @override
+  double price() {
+    return basePrice + (10 * toppings);
+  }
+}
+
+class Food extends MenuItem {
+  String size;
+  Food(super.name, super.basePrice, this.size);
+  @override
+  double price() {
+    switch (size) {
+      case 'S':
+        return basePrice * 1;
+      case 'M':
+        return basePrice * 1.2;
+      case 'L':
+        return basePrice * 1.5;
+    }
+    return basePrice;
+  }
+}
+
+//2.4
+class Wallet {
+  double? _balance;
+  double get balance => this.balance;
+  set balance(double input) {
+    if (input < 0) {
+      print('ผิดพลาด: ยอดเงินติดลบไม่ได้');
+    } else {
+      _balance = input;
+    }
+  }
+
+  bool pay(double baht) {
+    if (baht > balance) {
+      return false;
+    } else {
+      balance -= baht;
+      return true;
+    }
+  }
+}
+
+void main(List<String> args) {
+  //2.3
+  double sum = 0;
+  List<MenuItem> order = [
+    Drink('ลาเต้', 55, 1),
+    Food('ข้าวผัด', 60, 'L'),
+    Drink('อเมริกาโน่', 45, 0),
+  ];
+  for (MenuItem i in order) {
+    i.show();
+    sum += i.price();
+  }
+  print('ยอดเงินรวมทั้งสิ้น: $sum บาท');
+  print('---');
+
+  //2.4
+  Wallet().balance = -5;
+}
